5963d5cd8cd7b5125a467eb819503e8bc5c1430b
Steven Margolis <dischordiandesigns@gmail.com>
2016-01-03 22:24:23 -0800

    mainsceneadded

---
+++ FreeFallMobile/Assets/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    void FixedUpdate()
+    {
+        if (Mathf.Abs(Input.acceleration.x) > 0.05f)
+        {
+            this.gameObject.transform.position += new Vector3(Input.acceleration.x, 0, 0);
+        }
+       
+        if(Mathf.Abs(Input.acceleration.y) > 0.05f || Mathf.Abs(Input.acceleration.z) > 0.05f)
+        {
+            this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(Input.acceleration.y,Input.acceleration.z, 0) * 50.0f * Time.deltaTime);
+        }
+    }
+
+}
