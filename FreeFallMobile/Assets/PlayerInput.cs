b62b47660d31a6fe220eaa2469badf5fe59bab27
Steven Margolis <dischordiandesigns@gmail.com>
2016-01-04 21:21:45 -0800

    Tilt Controls
    Basis Tilt controls implemented.
    Steering left or right will move the character while tilting up and down
    will move the camera accordingly.

--- FreeFallMobile/Assets/PlayerInput.cs
+++ FreeFallMobile/Assets/PlayerInput.cs
@@ -3,17 +3,39 @@ using System.Collections;
 
 public class PlayerInput : MonoBehaviour
 {
+    public GameObject cam = null;
+
+    float vertRotationMultiplier = 40;
+    Vector3 startVector;
+    void Awake()
+    {
+        if (cam == null)
+            cam = GameObject.Find("Main Camera");
+    }
+    void Start()
+    {
+        startVector = new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
+    }
 
     void FixedUpdate()
     {
-        if (Mathf.Abs(Input.acceleration.x) > 0.05f)
+        Vector3 trueTilt = Vector3.zero;
+        trueTilt.x = Input.acceleration.x - startVector.x;
+        trueTilt.y = Input.acceleration.y - startVector.y;
+        trueTilt.z = Input.acceleration.z - startVector.z;
+        if (trueTilt.sqrMagnitude > 1)
+            trueTilt.Normalize();
+
+        if (Mathf.Abs(Input.acceleration.x) > 0.1f)
         {
-            this.gameObject.transform.position += new Vector3(Input.acceleration.x, 0, 0);
+            this.gameObject.transform.position += new Vector3(trueTilt.x, 0, 0);
         }
        
-        if(Mathf.Abs(Input.acceleration.y) > 0.05f || Mathf.Abs(Input.acceleration.z) > 0.05f)
+        if(Mathf.Abs(Input.acceleration.y) > 0.1f)
         {
-            this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(Input.acceleration.y,Input.acceleration.z, 0) * 50.0f * Time.deltaTime);
+            cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation,
+                                     Quaternion.Euler(new Vector3(trueTilt.y * vertRotationMultiplier
+                                     ,0, 0)),750.0f * Time.deltaTime);
         }
     }
 
