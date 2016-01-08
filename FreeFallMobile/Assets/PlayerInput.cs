830bbb8dee929ee589b1c78affe3c17f9fdd648b
Steven Margolis <dischordiandesigns@gmail.com>
2016-01-08 11:42:51 -0800

    Added in shootign

--- FreeFallMobile/Assets/PlayerInput.cs
+++ FreeFallMobile/Assets/PlayerInput.cs
@@ -5,7 +5,7 @@ public class PlayerInput : MonoBehaviour
 {
     public GameObject cam = null;
 
-    float vertRotationMultiplier = 40;
+    float vertRotationMultiplier = 60;
     Vector3 startVector;
     void Awake()
     {
@@ -26,16 +26,34 @@ public class PlayerInput : MonoBehaviour
         if (trueTilt.sqrMagnitude > 1)
             trueTilt.Normalize();
 
-        if (Mathf.Abs(Input.acceleration.x) > 0.1f)
+        if (Mathf.Abs(Input.acceleration.x) > 0.2f)
         {
             this.gameObject.transform.position += new Vector3(trueTilt.x, 0, 0);
         }
-       
-        if(Mathf.Abs(Input.acceleration.y) > 0.1f)
+        if (Mathf.Abs(Input.acceleration.y) > 0.05f)
         {
-            cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation,
-                                     Quaternion.Euler(new Vector3(trueTilt.y * vertRotationMultiplier
-                                     ,0, 0)),750.0f * Time.deltaTime);
+            this.gameObject.transform.position += new Vector3(0, -trueTilt.y, 0);
+            if (Mathf.Abs(Input.acceleration.y) > 0.2f)
+            {
+                cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation,
+                                         Quaternion.Euler(new Vector3(trueTilt.y * vertRotationMultiplier
+                                         , 0, 0)), 750.0f * Time.deltaTime);
+            }
+        }
+
+        
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.gameObject.GetComponent<BasicEnemy>())
+               {
+                  hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+               }
+            }
         }
     }
 
