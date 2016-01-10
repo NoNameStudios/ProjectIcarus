7275d89ba3009414b0edefb0b04a825ec41b57ee
Steven Margolis <dischordiandesigns@gmail.com>
2016-01-09 20:58:33 -0800

    .

--- FreeFallMobile/Assets/GameManagerScript.cs
+++ FreeFallMobile/Assets/GameManagerScript.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManagerScript : MonoBehaviour {
+public class GameManagerScript : MonoBehaviour
+{
 
-	// Use this for initialization
+	
 	void Awake ()
     {
         Screen.orientation = ScreenOrientation.LandscapeLeft;
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
