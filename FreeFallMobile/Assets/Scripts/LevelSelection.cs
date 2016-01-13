eaf649d818e5ae305c536f7e11c2f3dbfa9df9f8
Steven Margolis <dischordiandesigns@gmail.com>
2016-01-12 21:30:30 -0800

    prototype is done
    everything is absolutely terrible though and nearly unplayable.
    All systems work together now
    SO I need everyone to fix this up.

---
+++ FreeFallMobile/Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class LevelSelection : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+    public void LoadLevel(int scene)
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+}
