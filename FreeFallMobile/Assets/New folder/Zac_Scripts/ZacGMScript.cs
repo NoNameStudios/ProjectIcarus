68f4b57b195601a704f6c3d372050b38d81eab46
zac bogner <zac.bogner@gmail.com>
2016-01-13 16:02:18 -0800

    Zac Stuff
    Am i doing this right now?

---
+++ FreeFallMobile/Assets/New folder/Zac_Scripts/ZacGMScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZacGMScript : MonoBehaviour
+{
+    public static bool gameOver = false;
+
+    //public void PrimeQuit()
+    //{
+    //    Debug.log("Quitting Game");
+    //    Application.Quit();
+    //}
+
+    //public void ForceQuit()
+    //{
+    //    Application.Quit();
+    //}
+}
---
+++ FreeFallMobile/Assets/New folder/Zac_Scripts/musicController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*..........................................................*/
+// ****************Script Programmer******************//
+//****************************************************// 
+// ******************//Zac Bogner//*******************//
+/*..........................................................*/
+
+public class musicController : MonoBehaviour
+{
+    public AudioSource audio;
+    public AudioClip[] gameMusic = new AudioClip[3];
+    public float musicTimer = 0.0f;
+    public bool playingOpen = false;
+    public bool playingMain = false;
+    public bool playingEnd = false;
+    public bool mute = false;
+
+    public bool gameOver = false;
+    public GameObject gM;
+    ZacGMScript mC;
+
+    void Start()
+    {
+        audio.PlayOneShot(gameMusic[0]);
+        if(audio.isPlaying && musicTimer >= 78 && playingOpen)
+        {
+            audio.Stop();
+            playingMain = true;
+        }
+    }
+
+    public void Update()
+    {
+        musicTimer += Time.deltaTime;
+        if (audio.isPlaying && musicTimer >= 78 && playingOpen)
+        {
+            audio.Stop();
+            playingOpen = false;
+            audio.PlayOneShot(gameMusic[1]);
+            playingMain = true;
+        }
+        if (!audio.isPlaying && musicTimer >= 220 && !playingMain)
+        {
+            playingMain = false;
+            audio.Stop();
+            audio.PlayOneShot(gameMusic[2]);
+            playingMain = true;
+        }
+        if (mute)
+        {
+            audio.Stop();
+        }
+        if (ZacGMScript.gameOver == true && !playingEnd)
+        {
+            playingEnd = true;
+            audio.Stop();
+            audio.PlayOneShot(gameMusic[3]);
+        }
+    }
+}
+
+// 0 = gameStart Main Menu Music
+// 1 = Main In Game Music 1
+// 2 = Main In Game Music 2
+// 3 = gameEnd GameOver Music and Credits 
