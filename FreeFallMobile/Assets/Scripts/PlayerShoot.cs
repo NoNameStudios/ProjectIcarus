ecac10e3f0a1c1351246d6860ad43d4976c7ed88
bubbabrent <elwais12@gmail.com>
2016-01-29 01:16:26 -0800

    Flash shooting
    wasnt able to get pjone working so i used mouse click for testing. quick
    blue flash whenever the player shoots.

---
+++ FreeFallMobile/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject flasher;
+	// Use this for initialization
+	void Start ()
+    {
+        flasher.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine("Flash");
+        }
+	}
+
+    //Testing for shooting
+    
+        
+    
+
+    public IEnumerator Flash()
+    {
+        flasher.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        flasher.SetActive(false);
+
+    }
+}
